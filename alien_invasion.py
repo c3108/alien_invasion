2ca5b2732abd06e79522f856c817d5ab8d887b1b
Crystal Bevis <crystal.j.bevis@gmail.com>
2017-04-12 15:21:57 -0700

    Enable continuous movement through holding down keys and left movement. Speed movement up to 2

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,8 @@ def run_game():
 	while True:
 
 		gf.check_events(ship)
+		ship.update()
 		gf.update_screen(ai_settings, screen, ship, jeff)
+
 
 run_game()
--- ship.py
+++ ship.py
@@ -16,6 +16,18 @@ class Ship():
 		self.rect.centerx = self.screen_rect.centerx
 		self.rect.bottom = self.screen_rect.bottom
 
+		# Movement flag
+		self.moving_right = False
+		self.moving_left = False
+
+	def update(self):
+		"""Update the ship's position based on the movement flag."""
+		if self.moving_right:
+			self.rect.centerx +=2
+		if self.moving_left:
+			self.rect.centerx -=2
+
+
 	def blitme(self):
 		"""Draw teh ship at its current location."""
 		self.screen.blit(self.image, self.rect)
