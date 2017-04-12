d2c9796b38aeb315b4f1c51c29b30ce8fc95cf46
Crystal Bevis <crystal.j.bevis@gmail.com>
2017-04-12 10:50:38 -0700

    Changed scale of space ship and view screen

--- ship.py
+++ ship.py
@@ -7,7 +7,8 @@ class Ship():
 		self.screen = screen
 
 		# Load the ship image and get its rect.
-		self.image = pygame.image.load('images/ship.bmp')
+		self.icon = pygame.image.load('images/ship.bmp')
+		self.image = pygame.transform.scale(self.icon, (80, 80))
 		self.rect = self.image.get_rect()
 		self.screen_rect = screen.get_rect()
 
